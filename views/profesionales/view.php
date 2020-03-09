cb3f4146ada33c05cdeedefafe5a55be0b2a7c3a
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2020-03-09 10:59:48 +0100

    Enlaces en menu

--- views/profesionales/view.php
+++ views/profesionales/view.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = ['label' => 'Profesionales', 'url' => ['index']
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="profesionales-view">
+<div class="profesionales-view container">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
