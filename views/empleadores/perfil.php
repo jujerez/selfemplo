d43d8097424c9b0988ade17f2417de7d2fb22c28
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2020-04-11 18:31:44 +0200

    Perfil empleadores responsive

--- views/empleadores/perfil.php
+++ views/empleadores/perfil.php
@@ -29,7 +29,7 @@ $imagen = Url::to('@app/web/img/' . $model->usuario_id . '.jpg');
 <main class="empleadores-view container">
 
    <section class="row justify-content-around">
-       <aside class="col-md-3 col-sm-12 ">
+       <aside class="col-md-4 col-sm-12 col-lg-3">
             <div class="sidebar bg-light p-4 borde">
                 <div class="img-perfil text-center p-3">
                 
@@ -90,7 +90,7 @@ $imagen = Url::to('@app/web/img/' . $model->usuario_id . '.jpg');
             </div>
         </aside>
 
-       <div class="col-md-9 col-sm-12">
+       <div class="col-md-8 col-sm-12 col-lg-9">
             
             <ul class="nav nav-tabs" id="myTab" role="tablist">
                 <li class="nav-item">
